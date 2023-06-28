ee6106245ad18d2d6aa1403dfecd37c159e3ba82
rober <rober@DESKTOP-MN9AKPU>
2023-06-28 08:23:55 +0200

    Aggiungere i file di progetto.

---
+++ Taking the train/Program.cs
@@ -0,0 +1,56 @@
+﻿using Taking_the_train;
+
+Stations stations = new();
+
+Console.WriteLine("queste sono le varie fermate del treno:");
+
+LinkedList<string> ListOfStaion = new();
+ListOfStaion.AddFirst("Milano");
+ListOfStaion.AddLast("Napoli");
+ListOfStaion.AddLast("Venezia");
+ListOfStaion.AddLast("Firenze");
+ListOfStaion.AddLast("Otranto");
+
+foreach(string station in ListOfStaion)
+{
+    Console.WriteLine(station);
+}
+
+Console.WriteLine("\nscegli quale operazione vuoi effettuare \n1) aggiungi nuova nuona stazione/fermata;\n2) rimuovi una nuona stazione/fermata");
+int Input = int.Parse(Console.ReadLine());
+
+if (Input == 1)
+{
+    Console.Write("inserisci una nuova fermata: ");
+
+    string StopStation = stations.Add();
+    ListOfStaion.AddLast(StopStation);
+
+    Console.WriteLine("le fermate previste sono:");
+
+    foreach(string names in ListOfStaion)
+    {
+        Console.WriteLine(names);
+    }
+}
+else if (Input == 2)
+{
+    Console.WriteLine("le fermate previste sono:");
+
+    foreach (string names in ListOfStaion)
+    {
+        Console.WriteLine(names);
+    }
+
+    Console.WriteLine("scegli quale fermata eliminare dalla lista: ");
+
+    string NameStation = stations.Remove();
+    ListOfStaion.Remove(NameStation);
+
+    Console.WriteLine("le fermate previste sono:");
+
+    foreach (string names in ListOfStaion)
+    {
+        Console.WriteLine(names);
+    }
+}
---
+++ Taking the train/Stations.cs
@@ -0,0 +1,17 @@
+﻿namespace Taking_the_train
+{
+    internal class Stations
+    {
+        public string Add()
+        {
+            string InputString = Console.ReadLine();
+            return InputString;
+        }
+
+        public string Remove()
+        {
+            string InputString = Console.ReadLine();
+            return InputString;
+        }
+    }
+}
